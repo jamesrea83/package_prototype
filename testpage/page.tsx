0acb6ef52534b01c8bae563f2b71607c24800eb4
jamesrea83 <james@stream.bid>
2023-06-29 13:55:19 +0100

    scss test

--- testpage/page.tsx
+++ testpage/page.tsx
@@ -1,7 +1,13 @@
 import { NextPage } from 'next';
 
+import styles from './page.module.scss';
+
 const TestPage: NextPage = () => {
-	return <div>This is a test page exported as an NPM package</div>;
+	return (
+		<div className={styles.page}>
+			This is a test page exported as an NPM package
+		</div>
+	);
 };
 
 export default TestPage;
